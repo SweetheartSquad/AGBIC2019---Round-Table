901aced35ac5e9edbd5e9f104e115aae20a7c094
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-07-14 23:53:42 -0400

    more scene setup

--- src/assets/shader.frag.glsl
+++ src/assets/shader.frag.glsl
@@ -32,18 +32,33 @@ float noise(vec2 st) {
                      dot( random2(i + vec2(1.0,1.0) ), f - vec2(1.0,1.0) ), u.x), u.y);
 }
 
+const float posterize = 8.0;
 void main(void) {
 	vec2 suv = outTexCoord.xy / resolution.yx * resolution.x;
 
 	vec3 col = texture2D(uMainSampler, outTexCoord).rgb;
-	float brightness = max(col.g, col.r);//dot(col, vec3(0.299, 0.587, 0.114));
-	float r = step(noise(suv*resolution.x*0.33 + time/100.0)*.5+.5, col.r - (col.g + col.b) * 0.5);
+
+	float brightness = max(col.g, col.r);
+	vec2 rnoise = suv*resolution.x*0.25 + vec2(time*0.01)*(1.0-col.b);
+	float r = step(noise(rnoise)*.5+.5, col.r - col.g);
 	// force black/white
-	// vec3 bw = vec3(brightness);
-	vec3 bw = vec3(step(noise(suv*resolution.x*0.33 + time/100.0)*.5+.5, brightness));
+	vec2 bwnoise = suv.yx*resolution.x*0.25 + vec2(time*0.01)*(1.0-col.b);
+	vec3 bw = vec3(step(noise(bwnoise)*.5+.5, brightness));
 	// pick between red and black/white
 	col = mix(bw, vec3(1.0, 0.0, 0.0), r);
-	gl_FragColor = vec4(col*brightness, 1.0);
-	// gl_FragColor = vec4(vec3(col.r - (col.g + col.b) * 0.5), 1.0);
-	// gl_FragColor = vec4(vec3(r), 1.0);
+
+	col *= brightness;
+
+	// dither
+	vec3 raw = col;
+	vec3 posterized = raw - mod(raw, 1.0/posterize);
+
+	vec2 dit = floor(mod(outTexCoord * resolution, 4.0));
+	float limit = fract((gl_FragCoord.x+gl_FragCoord.y)*0.5)+0.5;
+	vec3 dither = step(limit, (raw - posterized) * posterize) / posterize;
+
+	col = vec3(posterized + dither);
+
+
+	gl_FragColor = vec4(col, 1.0);
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Phaser from "phaser";
+import frameImg from "./assets/frame.png";
 import swordImg from "./assets/sword.png";
 import fishImg from "./assets/fish.png";
 import fontImg from "./assets/font/font.png";
@@ -6,6 +7,7 @@ import fontXml from "!!file-loader!./assets/font/font.fnt";
 import './style.css';
 
 import Shader from './Shader';
+import EventText from "./EventText";
 
 const config = {
 	type: Phaser.AUTO,
@@ -14,8 +16,9 @@ const config = {
 		mode: Phaser.Scale.FIT,
 		autoCenter: Phaser.Scale.CENTER_BOTH,
 		width: 400,
-		height: 260,
+		height: 225,
 	},
+	roundPixels: true,
 	scene: {
 		preload,
 		create,
@@ -27,17 +30,26 @@ const game = new Phaser.Game(config);
 
 function preload() {
 	this.load.bitmapFont('font', fontImg, fontXml);
+	this.load.image("frame", frameImg);
 	this.load.image("sword", swordImg);
 	this.load.image("fish", fishImg);
 }
 
 function create() {
-	const fish = this.add.image(200, 100, "fish");
-	const sword = this.add.image(300, 100, "sword");
-
+	// setup post-processing
 	this.shader = this.game.renderer.addPipeline('Shader', new Shader(this.game));
 	this.shader.setFloat2('resolution', config.scale.width, config.scale.height);
 	this.cameras.main.setRenderToTexture(this.shader);
+	this.t = 0;
+
+	this.fish = this.add.image(269, 27, "fish");
+	this.fish.setOrigin(0);
+
+
+	this.eventText = new EventText(this);
+	this.frame = this.add.image(config.scale.width/2, config.scale.height/2, "frame");
+	this.sword = this.add.image(300, 100, "sword");
+
 
 	this.tweens.add({
 		targets: sword,
@@ -47,10 +59,25 @@ function create() {
 		yoyo: true,
 		loop: -1
 	});
-	this.t = 0;
+	
+
+
+	this.eventText.setText(`Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Fusce eu tempor elit. Mauris finibus, sapien a aliquam mattis, risus ex semper diam, vitae tincidunt felis mi at augue.
+
+Nulla.`);
+	//  Input events
+	this.x = 0;
+	this.y = 0;
+	this.input.on('pointermove', function(pointer) {
+		this.x = pointer.x;
+		this.y = pointer.y;
+	}, this);
 }
 
 function update() {
-	this.t += 1;
-	this.shader.setFloat1('time', this.t);
+	this.shader.setFloat1('time', ++this.t);
+	this.sword.x = this.x;
+	this.sword.y = this.y;
 }
